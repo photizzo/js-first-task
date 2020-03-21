8bc4709bf746a426f073305af99de9dfe35b926c
Emayoung <emayoung95@gmail.com>
2020-03-21 18:10:14 +0100

    Added script.js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const name = "Photizzo"
+const courses = ["HTML", "CSS", "HTML", "UI/UX"]
+
+// 1. If the number of courses you are doing is an odd number, find and display all odd numbers from 1-200 (inclusive)
+// 2. If the number of courses you are doing is an even number, find and display all even numbers from 1-200 (inclusive)
+
+const numbersToDisplay = []
+for (let i=1; i<= 200; i++) {
+    if (i%2 == 0) {
+        numbersToDisplay.push(i)
+    }
+}
+console.log(numbersToDisplay)
